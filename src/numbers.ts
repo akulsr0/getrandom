00a6506e243ad84b4e5408dce1a123cf550c250e
Mehmet Hakan Satman <mhsatman@gmail.com>
2020-04-10 15:30:02 +0300

    functions for sampling N numbers with or without replace

--- src/numbers.ts
+++ src/numbers.ts
@@ -12,3 +12,22 @@ export const getRandomBetween = (min: number, max: number): number => {
 export const getRandomFloatBetween = (min: number, max: number): number => {
   return Math.random() * (max - min + 1) + min;
 };
+
+export const getNRandomBetweenWithReplace = (n: number, min: number, max: number): Array<number> => {
+  let numbers: Array<number> = new Array<number>(n);
+  for (var i = 0; i < n; i++) {
+    numbers[i] = getRandomBetween(min, max);
+  }
+  return numbers;
+};
+
+export const getNRandomBetweenWithoutReplace = (n: number, min: number, max: number): Array<number> => {
+  let numbers: Array<number> = new Array<number>();
+  while (numbers.length < n) {
+    let currentNum = getRandomBetween(min, max);
+    if (!numbers.includes(currentNum)) {
+      numbers.push(currentNum);
+    }
+  }
+  return numbers;
+};
