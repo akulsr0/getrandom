26e5f0408bbacc4ddd6dc5bbd8e8ebfd20fc5f62
Lakshy <lakshy.8192211@gmail.com>
2020-05-09 14:51:23 +0530

    getRandomDate

--- src/date.ts
+++ src/date.ts
@@ -5,7 +5,7 @@ export const getDateBetween = (start: Date, end: Date): Date => {
 };
 
 export const getRandomDate = () => {
-  var from = new Date();
+  var from = new Date(3000, 8, 26);
   var to = new Date(1947, 8, 26);
   var randomDate = from.getTime() + Math.random() * (to.getTime() - from.getTime());
   return new Date(randomDate);
